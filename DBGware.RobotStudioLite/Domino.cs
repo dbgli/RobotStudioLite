eea6e87a09dbb69bb39ef88ef0ad8696ca948b17
dbgli <dbgli@outlook.com>
2023-08-02 22:41:51 +0800

    实现Domino类

---
+++ DBGware.RobotStudioLite/Domino.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBGware.RobotStudioLite
+{
+    public class Domino
+    {
+        public int ID { get; set; }
+        public DominoSize Size { get; set; } = new();
+        public DominoPosition Position { get; set; } = new();
+    }
+}
---
+++ DBGware.RobotStudioLite/DominoPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBGware.RobotStudioLite
+{
+    public class DominoPosition
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double R { get; set; }
+    }
+}
