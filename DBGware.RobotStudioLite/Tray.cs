a90c4d6a3aac0e88e962e6e58abc4a5ccd3e9508
dbgli <dbgli@outlook.com>
2023-08-04 19:45:58 +0800

    优化DominoesTaskPanelSettingsTab类属性

---
+++ DBGware.RobotStudioLite/Tray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBGware.RobotStudioLite
+{
+    public class Tray
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public double RowSpacing { get; set; }
+        public double ColumnSpacing { get; set; }
+    }
+}
