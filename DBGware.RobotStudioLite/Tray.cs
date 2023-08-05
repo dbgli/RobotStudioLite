017979df5450adcbb3c15d058309f913a3c5e7b6
dbgli <dbgli@outlook.com>
2023-08-05 13:29:31 +0800

    为多米诺队列ListView添加ComboBox

--- DBGware.RobotStudioLite/Tray.cs
+++ DBGware.RobotStudioLite/Tray.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 
 namespace DBGware.RobotStudioLite
 {
     public class Tray
     {
+        private readonly ObservableCollection<int> ids = new();
+
         public int Rows { get; set; }
         public int Columns { get; set; }
         public double RowSpacing { get; set; }
         public double ColumnSpacing { get; set; }
+        public ObservableCollection<int> IDs
+        {
+            get
+            {
+                if (ids.Count == Rows * Columns) return ids;
+                ids.Clear();
+                for (int id = 1; id <= Rows * Columns; id++) ids.Add(id);
+                return ids;
+            }
+        }
     }
 }
