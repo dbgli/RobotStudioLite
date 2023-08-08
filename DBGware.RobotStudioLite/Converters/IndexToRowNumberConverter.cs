7ae049076f7bdf633d004fb2ae2fdd7ab146580c
dbgli <dbgli@outlook.com>
2023-08-08 11:41:19 +0800

    添加行号列

---
+++ DBGware.RobotStudioLite/Converters/IndexToRowNumberConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace DBGware.RobotStudioLite.Converters
+{
+    public class IndexToRowNumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not ListViewItem listViewItem) return string.Empty;
+            ListView listView = (ListView)ItemsControl.ItemsControlFromItemContainer(listViewItem);
+            int rowNumber = listView.ItemContainerGenerator.IndexFromContainer(listViewItem) + 1;
+            return rowNumber.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
