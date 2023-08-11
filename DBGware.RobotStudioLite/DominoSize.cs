d0595ba221596ccf9b281bdac38ecf383b9bcc21
dbgli <dbgli@outlook.com>
2023-08-11 17:28:24 +0800

    实现DominoSize类INotifyPropertyChanged接口

--- DBGware.RobotStudioLite/DominoSize.cs
+++ DBGware.RobotStudioLite/DominoSize.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace DBGware.RobotStudioLite
 {
-    public class DominoSize
+    public class DominoSize : INotifyPropertyChanged
     {
-        public double Length { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get => length;
+            set
+            {
+                if (length == value) return;
+                length = value;
+                OnPropertyChanged(nameof(Length));
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (width == value) return;
+                width = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (height == value) return;
+                height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new(propertyName));
+        }
     }
 }
