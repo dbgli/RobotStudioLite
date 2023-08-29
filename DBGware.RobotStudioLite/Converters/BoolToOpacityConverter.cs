33fd6c8b6d346135b427772f4fb71dbdf2e587c4
dbgli <dbgli@outlook.com>
2023-08-29 16:17:49 +0800

    实现当模型不可见时降低对应图标的不透明度

---
+++ DBGware.RobotStudioLite/Converters/BoolToOpacityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DBGware.RobotStudioLite.Converters
+{
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool isVisible) return 0.0;
+            return isVisible ? 1.0 : 0.5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
